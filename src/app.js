2165cf3f3c8822ce865b3cfea90fbcc72a5cdf46
Chris Kozlowski <kozlowskicd@gmail.com>
2018-12-06 21:51:18 -0800

    refactor for 1 player

--- src/app.js
+++ src/app.js
@@ -26,192 +26,134 @@ io.sockets.on('connection', (socket) => {
   // when someone disconnects
   socket.on('disconnect', () => {
     socket.removeAllListeners();
-    console.log(`${socket.username} has left the game`);
+    console.log(`${socket.id} has left the game`);
   });
   
   // handles logins for new and returning clients.  Expected input: (Object) {username: 'username', password: 'password', email: 'email'}
-  // socket.on('sign-in', (userObj) => {
-    
-       
-  // superagent.post(`${process.env.API_URL}/signup`)
-  //   .send(JSON.stringify(userObj))
-  //   .set('Content-Type', 'application/json')
-  //   .then(data => {
-  //     userObj.auth = data.text;
-  //     socket.emit('signed-in-newuser', userObj.username);
-  //     console.log(`${userObj.username} has signed up and signed in`);
-  //   })
-  //   .catch(error => {
-  //     if (error) {
-  //       console.log('Error signing in', error);
-  //     }
-
-  //         socket.emit('signing-in');
-  //         superagent.post(`${process.env.API_URL}/signin`)
-  //           .send(`{'username' : ${socket.username.toString()}, 'password' : ${socket.password.toString()}`)
-  //           .set('Content-Type', 'application/json')
-  //           .then(data => {
-  //             socket.auth = data.req.headers.auth;
-  //             socket.emit('signed-in', socket.username);
-  //             console.log(`Returning user ${socket.username} has signed in`);
-  //           })
-  //           .catch(error => {
-  //             if (error) {
-  //               console.log('Error signing in');
-  //             }
-  //           });
-  //       }
-  //     })
-  //     .catch(err => console.log(err));
-  // });
-
+  socket.on('sign-in', (userObj) => {
+    console.log(userObj);
+    superagent.post(`https://${userObj.username}:${userObj.password}@enseven-api-service.herokuapp.com/signin`)
+      .set('Content-Type', 'application/json')
+      .then(data => {
+        userObj.auth = data.text;
+        socket.emit('signed-in-user', userObj);
+        console.log(`${userObj.username} has signed up and signed in`);
+      })
+      .catch(error => {
+        console.log('Error signing in', error);
+      });
+  });
       
   socket.on('sign-up', (userObj) => {
     superagent.post(`${process.env.API_URL}/signup`)
       .send(JSON.stringify(userObj))
       .set('Content-Type', 'application/json')
       .then(data => {
+        console.log(data);
         userObj.auth = data.text;
-        socket.emit('signed-in-newuser', userObj.username);
+        // console.log(userObj);
+        socket.emit('signed-in-newuser', userObj);
         console.log(`${userObj.username} has signed up and signed in`);
-      });
+      })
+      .catch(err => console.log(err));
   });
   
   // After a client signs in, joins a new game.  Waits for two clients before starting the game
   // Expected input: (String) 'username'
   socket.on('join', (thisPlayer) => {
-    console.log(thisPlayer, ' joined');
-    if (Game.players === 0 && Game.isThereTwoPlayers === false) {
+
+    if (Game.players === 0) {
       Game.player1 = {
-        username: thisPlayer,
+        username: thisPlayer.username,
+        auth: thisPlayer.auth,
+        player: 'one',
+        didIWin: undefined,
       };
       Game.players++;
-      console.log('Player One joined: ', Game.player1.username);
-      // socket.join(`player${Game.players}`);
-      socket.join('player1');
-      socket.emit('player1-joined', thisPlayer);
+      // console.log('Player One joined: ', Game.player1);
+      socket.emit('player1-joined', Game.player1);
     }
-    else if (Game.players === 1 && Game.isThereTwoPlayers === false) {
-      Game.player2 = {
-        username: thisPlayer,
-      };
-      Game.players++;
-      console.log('Player Two joined: ', Game.player2.username);
-      // socket.join(`player${Game.player1.username}`);
-      Game.isThereTwoPlayers = true;
-      socket.join('player2');
-      socket.emit('player2-joined', Game.player2.username);
-      // io.emit('ready-to-play', 'Game ready to begin!');
-      // At this point waiting to hear 'play' emit from two clients
-    }
-    else if (Game.isThereTwoPlayers === true) {
-      Game.spectators.push(JSON.stringify(thisPlayer).toString());
-      console.log('spectators: ', Game.spectators);
-      spectators.emit('spectate', Game.spectators);  // Dunno if this'll work out, makes an array of all who join after the first two needed to start a game.
+    // else if (Game.players === 1 && Game.isThereTwoPlayers === false) {
+    //   Game.player2 = {
+    //     username: thisPlayer.username,
+    //     auth: thisPlayer.auth,
+    //     player: 'two',
+    //   };
+    //   Game.players++;
+    //   // console.log('Player Two joined: ', Game.player2);
+    //   // socket.join(`player${Game.player1.username}`);
+    //   Game.isThereTwoPlayers = true;
+    //   socket.emit('player2-joined', Game.player2);
+    //   // io.emit('ready-to-play', 'Game ready to begin!');
+    //   // At this point waiting to hear 'play' emit from two clients
+    // }
+    else {
+      console.log('Something went wrong determining player');
     }
   });
   
   //  Runs game related functions from game-engine.js
   // wait for both players before starting
   socket.on('play', () => {
-    // console.log('play triggered');
-    // playCount++;
-    // console.log(playCount);
-    // if (playCount == 2) {
-    console.log('players joined, game starting');
-    //  Input is an array of two player objects
-    let bothPlayerObjs = [Game.player1, Game.player2];
-    //  This loop runs until the game is completed or the players quit
-    while(Game.gameover !== true) {
-      Game.playerTracker = 1;
-      __getClientInput();
-      Game.applyInput(Game.currentInput);
-      Game.playerTracker = 2;
-      __getClientInput();
-      Game.applyInput(Game.currentInput);
-
-    }
-    bothPlayerObjs.forEach((player) => {
-      __determineWinner(player);
+    console.log(Game.gameover);
+    socket.emit('input-request');
+    socket.on('input', input => {
+      Game.applyInput(input);
+      console.log('input received,', input);
+      socket.emit('results', 'player1 results');
+      __determineWinner(Game.player1);
+      Game.endSession();
     });
-    //  Function for dealing with a completed game
-    //  Closing out the game session
-    Game.endSession();  //  If spectating makes it in, this should pass in spectators array as a second parameter as well.
-    
-    // }
   });
-  socket.on('get-stats', (socket) => {
-    superagent.get(`${process.env.API_URL}/playerstats/${socket}`)
-      .then(results => {
-        let stats = {
-          username: results.name,
-          wins: results.wins,
-          losses: results.losses,
-        };
-        players.in(`player${socket}`).emit('stats', stats)
-          .catch(err => console.log(err));
-      });
-  });
-
+  
   // Listens for quit event from either client during play.  Will confirm the quit with both players
-  players.on('quit-game', () => {
-    Game.quit();
+  socket.on('quit-game', () => {
+    // get both players to send quit events 
+    let quitCount = 0;
+    socket.emit('confirm-quit');
+    socket.on('quit-confirmed', () => {
+      quitCount++;
+    });
+    while(quitCount === 2) {
+      Game.endSession();
+      socket.emit('end');
+      socket.removeAllListeners();
+      quitCount = 0;
+    }
     players.emit('end');
   });
-  // spectators.broadcast.emit('watch-broadcast', (socket) => {  
-  // Not focusing on this now, still need to figure out what to pass through
-  //   socket.players = [Game.player1.username, Game.player2.username];
-  //   socket.spectators = [Game.spectators];
-  // });
-  // spectators.on('leave-game', (socket) => {
-  //   // TODO
-  // });
-  // });
-  const  __getClientInput = () => {
-    if(Game.playerTracker === 1) {  
-      console.log('player1\'s turn');
-      socket.emit('input-request-p1');
-      socket.on('input-p1', (input) => {
-        console.log('Player 1 input:', input);
-        Game.currentInput = input.toString();
-      });
-    }
-    if(Game.playerTracker === 2) {
-      console.log('player2\'s turn');  
-      socket.emit('input-request-p2');
-      socket.on('input-p2', (input) => {
-        console.log('player 2 input:', input);
-        Game.currentInput = input.toString();
-      });
-    }
-  };
+  
   const __determineWinner = (player) => {
+    console.log(player);
     if(player.didIWin === true) {
       console.log(`${player.username} won, storing results...`);
-      superagent.post(`${process.env.API_URL}/singlestat`)
-        .send(JSON.stringify({name: player.username, win: player.didIWin}))
+      superagent.post(`${process.env.API_URL}/api/v1/singlestat`)
+        .send({name: player.username, win: player.didIWin})
+        .set('Authorization', `Bearer ${player.auth}`)
         .then(() => {
-          console.log(`Results saved for ${player.username}`);  
-          socket.in(`${player.username}`).emit('won');
+          socket.emit('won');
+          console.log(`ASYNC: Results saved for ${player.username}`);  
           player.didIWin = undefined;
         })
         .catch(err => console.log(err));
     }
     else if(player.didIWin === false) {
       console.log(`${player.username} lost, storing results...`);
-      superagent.post(`${process.env.API_URL}/singlestat`)
-        .send(JSON.stringify({name: player.username, win: player.didIWin}))
+      superagent.post(`${process.env.API_URL}/api/v1/singlestat`)
+        .send({name: player.username, win: player.didIWin})
+        .set('Authorization', `Bearer ${player.auth}`)
         .then(() => {
-          console.log(`Results saved for ${player.username}`);
+          console.log(`ASYNC: Results saved for ${player.username}`);
           player.didIWin = undefined;
-          socket.in(`${player.username}`).emit('lost');
+          socket.emit('lost');
         })
-        .catch(err => console.log(err));
+        .catch(err => console.log('3'));
     }
     else {
       console.log('Something went wrong determining the winner');
     }
   };
+  
 });
 
 
--- src/lib/game-engine.js
+++ src/lib/game-engine.js
@@ -1,7 +1,5 @@
 'use strict';
 
-const superagent = require('superagent');
-
 const Game = {
   gameover: false,
   isThereTwoPlayers: false,
@@ -17,63 +15,11 @@ const Game = {
   applyInput: (input) => {
     // The parameter is the input from the current player client.  Use this as the inputs for the game.
     Game.inputLog.push(input);
-    Game.__game(input);
-  },
-
-  //  The first to join is player1, the next is player2, all others after that are spectators
-  joinGame: (socket) => {
-    console.log('testing', Game.players);
-    if (Game.players === 0 && Game.isThereTwoPlayers === false) {
-      Game.player1 = {
-        username: socket,
-      };
-      Game.players++;
-      console.log('Player One joined: ', Game.player1.username);
-      // socket.join(`player${Game.players}`);
-      socket.emit('player1-joined', username);
-    }
-    else if (Game.players === 1 && Game.isThereTwoPlayers === false) {
-      Game.player2 = {
-        username: socket,
-      };
-      Game.players++;
-      console.log('Player Two joined: ', Game.player2.username);
-      // socket.join(`player${Game.player1.username}`);
-      Game.isThereTwoPlayers = true;
-      socket.emit('player2-joined', Game.player2.username);
-      // io.emit('ready-to-play', 'Game ready to begin!');
-      // At this point waiting to hear 'play' emit from two clients
-    }
-    else if (Game.isThereTwoPlayers === true) {
-      Game.spectators.push(JSON.stringify(socket.req.data).username.toString());
-      console.log('spectators: ', Game.spectators);
-      spectators.emit('spectate', Game.spectators);  // Dunno if this'll work out, makes an array of all who join after the first two needed to start a game.
-    }
+    console.log('playing the game...', input);
+    Game.player1.didIWin = true;
+    Game.gameover = true;
   },
       
-  // Takes the player1 and player2 objects in an array
-  // Determines and notifies winner and loser, and posts stats to API server
-  determineWinner: (bothPlayers) => {
-    bothPlayers.forEach((player) => {
-      
-    });
-  },
-
-  quit: () => {
-    // get both players to send quit events 
-    let quitCount = 0;
-    socket.emit('confirm-quit');
-    socket.on('quit-confirmed', () => {
-      quitCount++;
-    });
-    while(quitCount === 2) {
-      Game.endSession();
-      io.emit('end');
-      io.removeAllListeners();
-      quitCount = 0;
-    }
-  },
-
   endSession: () => {
     //  Dump all session data
     Game.isThereTwoPlayers = false;
@@ -81,21 +27,10 @@ const Game = {
     Game.player1 = {};
     Game.player2 = {};
     Game.spectators = [];
+    console.log('Game session cleared.');
   },
 
   ///////////////////////  GAME LOGIC  ///////////////////
-  __game: (input) => {
-    // Actual game logic goes here
-    // If we're lucky it can be easily modularized to other files
-    // All this is test stuff
-    console.log('playing the game...');
-    Game.testGame++;
-    if(Game.testGame === 5) {
-      Game.player1.didIWin = false;
-      Game.player2.didIWin = true;
-      Game.gameover = true;
-    }
-  },
-};
 
+};
 module.exports = Game;
